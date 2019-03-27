6283bae01c7e751fbf02cfa1df75e09d1c49c971
Flaviano Dias <flavindias@msn.com>
2019-03-27 10:44:29 -0300

    base project

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+var App = React.createClass({
+  render: function () {
+    return <h1>Aplicação</h1>
+  }
+})
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpack = require('webpack')
+const validate = require('webpack-validator')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'), 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: { 
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+   plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
