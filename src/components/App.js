d0a7d4076faad133e7ac5b45cc76f586f9766f98
Flaviano Dias <flavindias@msn.com>
2019-04-13 11:13:45 -0300

    Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import Footer  from '../components/Footer/Footer';
 
 export default class App extends React.Component {
     render(){
        return(
            <div>
                <Header/>
-               <h1> the list  </h1>
+               <Footer/>
+
            </div>   
         )
      }
